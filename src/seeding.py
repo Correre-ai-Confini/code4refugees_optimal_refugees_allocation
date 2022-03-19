a0c89fd1ea6c298a44bed4d1dcfd5c937c3720c7
404answernotfound <404answernotfound.public@gmail.com>
2022-03-19 16:35:57 +0100

    added new faker methods and extended faker class with nationality and religion for seeding

--- src/seeding.py
+++ src/seeding.py
@@ -1,7 +1,16 @@
 from occupant import Occupant
 from faker import Faker
+from random import choice
 
-faker = Faker()
+class _Faker(Faker):
+    def religion(self):
+        return choice(['Christian', 'Muslim', 'Orthodox'])
+    def nationality(self):
+        # TODO Add more nationalities
+        return choice(['Ucranian', 'Belarus', 'Russian', 'Romanian', 'Italian', 'Chinese'])
+
+
+faker = _Faker()
 
 people = []
 
@@ -11,8 +20,8 @@ for i in range(1, 1000):
         surname = faker.last_name(),
         birth = faker.past_date(),
         gender = faker.pybool(),
-        nationality = faker.country(),
-        religion = faker.country(),
+        nationality = faker.nationality(),
+        religion = faker.religion(),
         relatives = [],
         triage = faker.pyint(0, 4)
     ))
